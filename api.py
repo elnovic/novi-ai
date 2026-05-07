351a229669daf31808a9e040bf00f54eae72bd33
elnovic <elnovicmalonda2@gmail.com>
2026-05-07 02:15:32 +0100

    Fix CORS et RLS

--- api.py
+++ api.py
@@ -17,7 +17,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
+    allow_credentials=True
 )
 
 # Client Supabase
